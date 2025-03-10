015db49701793ffba7261c919743523803a743ee
dryingpaint <melissa@altera.al>
2025-03-10 16:09:30 -0700

    update ml take home

---
+++ main.py
@@ -0,0 +1,43 @@
+import transformers as tr
+
+amateur_path = "Qwen/Qwen2.5-Coder-0.5B-Instruct"
+expert_path = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
+
+tokenizer = tr.AutoTokenizer.from_pretrained(amateur_path)
+
+user_message = """Give a very very brief docstring for the following function:\n```\nfunction updateEloScores(
+	scores,
+	results,
+	kFactor = 4,
+) {
+	for (const result of results) {
+		const { first, second, outcome } = result;
+		const firstScore = scores[first] ?? 1000;
+		const secondScore = scores[second] ?? 1000;
+
+		const expectedScoreFirst = 1 / (1 + Math.pow(10, (secondScore - firstScore) / 400));
+		const expectedScoreSecond = 1 / (1 + Math.pow(10, (firstScore - secondScore) / 400));
+		let sa = 0.5;
+		if (outcome === 1) {
+			sa = 1;
+		} else if (outcome === -1) {
+			sa = 0;
+		}
+		scores[first] = firstScore + kFactor * (sa - expectedScoreFirst);
+		scores[second] = secondScore + kFactor * (1 - sa - expectedScoreSecond);
+	}
+	return scores;
+}\n```"""
+
+prompt = tokenizer.apply_chat_template(
+    [
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": user_message},
+    ],
+    add_generation_prompt=True,
+    tokenize=False,
+)
+
+
+def contrastive_generation(amateur, expert, prompt, max_tokens) -> str:
+    return ""
